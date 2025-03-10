e5d61ccbfe714f52356b400e09fb81a73b362117
Manthan Singla <manthansingla786@gmail.com>
2025-03-11 05:18:06 +0530

    fixed alphabet screen

--- app/src/main/java/com/example/arlearner/ui/screens/alphabetscreen.kt
+++ app/src/main/java/com/example/arlearner/ui/screens/alphabetscreen.kt
@@ -1,5 +1,6 @@
 package com.example.arlearner.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +27,7 @@ import androidx.navigation.NavController
 import com.example.arlearner.ui.navigation.ARSCREEN
 import kotlin.random.Random
 
+
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun alphabetscreen(navController: NavController) {
@@ -32,51 +35,53 @@ fun alphabetscreen(navController: NavController) {
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     )
-
     Column {
         Box(modifier = Modifier.height(60.dp)) {
             Text(text = "Alphabets", fontSize = 24.sp, modifier = Modifier.align(Alignment.Center))
         }
-
         FlowRow(
-            modifier = Modifier.fillMaxWidth().verticalScroll(
-                state = rememberScrollState()
-            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(
+                    state = rememberScrollState()
+                ),
             verticalArrangement = Arrangement.Center,
             horizontalArrangement = Arrangement.Center
         ) {
             listOfAlphabets.forEach { alphabet ->
-                alphabetitem(alphabet) {
+                AlphabetItem(alphabet = alphabet) {
                     navController.navigate(ARSCREEN(alphabet))
                 }
             }
         }
     }
-
-
 }
 
 @Composable
-fun alphabetitem(alphabet: String, onClick: () -> Unit) {
+fun AlphabetItem(alphabet: String, onClick: () -> Unit) {
     val color = remember(alphabet) {
-        genrandomlightcolor()
+        generateRandomLightColor()
     }
-    Box(
-        modifier = Modifier
-            .size(60.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .clickable { onClick() }
-    ) {
-        Text(text = alphabet, fontSize = 24.sp, modifier = Modifier.align(Alignment.Center))
+    Box(modifier = Modifier
+        .padding(16.dp)
+        .size(60.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(color)
+        .clickable { onClick() }) {
+        Text(
+            text = alphabet,
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.Center),
+            color = Color.Black
+        )
     }
 }
 
-fun genrandomlightcolor(): Color {
+fun generateRandomLightColor(): Color {
     val random = Random(System.currentTimeMillis())
     val red = random.nextInt(150, 256)
     val green = random.nextInt(200, 256)
     val blue = random.nextInt(200, 256)
-
     val color = Color(red, green, blue)
     return color
 }
