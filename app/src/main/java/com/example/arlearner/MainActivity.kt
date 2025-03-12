e1fc8754af00fb8952d2f4be8b26534e060f0edb
Manthan Singla <manthansingla786@gmail.com>
2025-03-12 22:08:44 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/arlearner/MainActivity.kt
+++ app/src/main/java/com/example/arlearner/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable<ALPHABETSCREEN> {
-                            alphabetscreen(navController)
+                            alphabetscreen(navController) 
                         }
                     }
                 }
